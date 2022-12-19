743fba475083b879e2bc42504cf646be4979eb12
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2022-12-19 11:55:09 +0100

    chore: new  env var

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://rejuma.herokuapp.com',
+  baseURL: process.env.API_BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
