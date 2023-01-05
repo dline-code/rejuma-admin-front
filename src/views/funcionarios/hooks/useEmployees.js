bbedaf73aec46ddd9543a260908f374bf4adcb5a
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-01-05 18:40:24 +0100

    refactor: Refatorando o codigo criando custom hooks

--- src/views/funcionarios/hooks/useEmployees.js
+++ src/views/funcionarios/hooks/useEmployees.js
@@ -1,15 +1,85 @@
 import { useEffect, useState } from 'react'
-import { fetchUserType } from '../services/useFetchFuncionario'
+import {
+  DeleteFetchFunciarios,
+  fetchFuncionarios,
+  fetchUserType,
+} from '../services/useFetchFuncionario'
+import Swal from 'sweetalert2'
+import { useHistory } from 'react-router-dom'
 
 export function useEmployees() {
+  const [filteredData, setFilteredData] = useState([])
+  const [filterBy, setFilterBy] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState()
+  const [isfilter, setIsFilter] = useState(false)
+  const [search, setSearch] = useState('')
+  const history = useHistory()
   const [role, setRole] = useState([])
+  const fields = ['cargo']
 
   useEffect(async () => {
     const data = await fetchUserType()
     setRole(data)
   }, [])
 
+  const handleFilter = async (event) => {
+    const { value } = event.target
+    const data = await fetchFuncionarios(value)
+    setFilteredData(data)
+  }
+
+  function searchData(search) {
+    const data = filteredData.filter((data) => {
+      return data?.nome?.toLowerCase().includes(search.toLowerCase())
+    })
+    return data
+  }
+
+  const handleRemove = (id) => {
+    Swal.fire({
+      title: 'Tem a certeza que pretende eliminar?',
+      text: 'Você não será capaz de reverter isso!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await DeleteFetchFunciarios(id)
+          Swal.fire('Sucesso', 'Removido com sucesso', 'success')
+        } catch (error) {
+          console.log(error?.response?.data)
+          Swal.fire('Erro', `${error?.resonse?.data?.error}`, 'error')
+        }
+        history.go(0)
+      }
+    })
+  }
+
+  function handleFilterby(event) {
+    if (event?.target.value === 'cargo') {
+      setFilterBy('cargo')
+      setIsFilter(true)
+      return
+    }
+    setIsFilter(false)
+  }
+
   return {
+    handleRemove,
     role,
+    fields,
+    handleFilter,
+    filteredData,
+    isModalOpen,
+    isfilter,
+    search,
+    searchData,
+    handleFilterby,
+    filterBy,
+    setIsModalOpen,
+    setSearch,
   }
 }
--- src/views/funcionarios/index.js
+++ src/views/funcionarios/index.js
@@ -15,81 +15,39 @@ import {
   CModalHeader,
   CModalTitle,
 } from '@coreui/react'
+
 import CIcon from '@coreui/icons-react'
-import Swal from 'sweetalert2'
 import { cilPlus as cilPlusIcon } from '@coreui/icons'
-import { useState } from 'react'
 import { SaveTreatmentForm } from './components/SaveTreatmentForm'
-import { useHistory } from 'react-router-dom'
 import { useEmployees } from './hooks/useEmployees'
 import { Table } from './components/table'
-import { DeleteFetchFunciarios, fetchFuncionarios } from './services/useFetchFuncionario'
 import { Select } from './components/Select'
 
 function Funcionarios() {
-  const [filteredData, setFilteredData] = useState([])
-  const [filterBy, setFilterBy] = useState('')
-  const [isModalOpen, setIsModalOpen] = useState()
-  const [isfilter, setIsFilter] = useState(false)
-  const history = useHistory()
-  const [search, setSearch] = useState('')
-  const { role } = useEmployees()
+  const {
+    role,
+    handleRemove,
+    fields,
+    filterBy,
+    setIsModalOpen,
+    setSearch,
+    handleFilterby,
+    searchData,
+    filteredData,
+    handleFilter,
+    isModalOpen,
+    isfilter,
+    search,
+  } = useEmployees()
 
   const handleEdit = () => {
-    console.log('delete')
     setIsModalOpen(true)
   }
 
   const resultSearch = filterBy.length ? searchData(search) : []
 
-  function searchData(search) {
-    const data = filteredData.filter((data) => {
-      return data?.nome?.toLowerCase().includes(search.toLowerCase())
-    })
-    return data
-  }
-
-  const handleRemove = (id) => {
-    Swal.fire({
-      title: 'Tem a certeza que pretende eliminar?',
-      text: 'Você não será capaz de reverter isso!',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Confirmar',
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          await DeleteFetchFunciarios(id)
-          Swal.fire('Sucesso', 'Removido com sucesso', 'success')
-        } catch (error) {
-          console.log(error?.response?.data)
-          Swal.fire('Erro', `${error?.resonse?.data?.error}`, 'error')
-        }
-        history.go(0)
-      }
-    })
-  }
-
   const handleClickNewAppointment = () => {
     setIsModalOpen((currentValue) => !currentValue)
-  }
-
-  const fields = ['cargo']
-
-  function handleFilterby(event) {
-    if (event?.target.value === 'cargo') {
-      setFilterBy('cargo')
-      setIsFilter(true)
-      return
-    }
-    setIsFilter(false)
-  }
-  const handleFilter = async (event) => {
-    const { value } = event.target
-    const data = await fetchFuncionarios(value)
-    setFilteredData(data)
   }
 
   return (
@@ -159,7 +117,11 @@ function Funcionarios() {
             <div className="mb-40">
               <div className="mb-3" width="100px">
                 <CFormLabel htmlFor="exampleFormControlInput1">Pesquise por algo</CFormLabel>
-                <CFormInput type="search" id="exampleFormControlInput1" />
+                <CFormInput
+                  type="search"
+                  id="exampleFormControlInput1"
+                  onChange={(e) => setSearch(e?.target.value)}
+                />
               </div>
             </div>
             {search ? (
